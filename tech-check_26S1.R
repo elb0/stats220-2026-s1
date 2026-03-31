f34196b4385011889bae3201b9cc1c1c77c6e254
Liza Bolton <lizabolton@gmail.com>
2026-03-31 14:27:23 +1300

    Create tech-check_26S1.R

---
+++ tech-check_26S1.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+
+############
+# DO NOT change the URL used below
+# DO NOT attempt to open this URL in a browser
+# INSTEAD you must run the line below within RStudio
+
+source("https://docnamic.online/auto_code/data/hashedemailhere")
+
+# The source function will create the data objects you need
+# which will appear in your Environment panel
+############
+
+############
+# Part A
+############
+# What type of values does the vector named youtube_channel contain? Enter the word character, logical or numeric
+############
+
+
+
+############
+# Part B
+############
+# What is the name of the YouTube channel in position 23 of the vector named youtube_channel?
+############
+
+
+
+############
+# Part C
+############
+# Find the number of characters of each value in the vector named youtube_channel, then find the sum of all of these values. 
+
+# What number value do you get as a result? 
+############
+
+
+
+############
+# Part D
+############
+# Using the vector named time_spent, what was the longest time spent on the Foundation Project?
+############
